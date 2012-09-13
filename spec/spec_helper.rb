2898cb4ecf5407cccd99c6a5c672b592fac75569
Chris Beer <cabeer@stanford.edu>
2012-09-13 08:55:49 -0400

    add simplecov coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
-require 'rspec'
+if ENV['COVERAGE'] and RUBY_VERSION =~ /^1.9/
+  require 'simplecov'
+  SimpleCov.start
+end
+
+require 'rspec/autorun'
 require 'nom'
+
+RSpec.configure do |config|
+
+end
 
