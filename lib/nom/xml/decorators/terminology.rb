6bbc20feae3f52bbb29f5102018895c86aa75a89
Chris Beer <cabeer@stanford.edu>
2012-09-14 11:11:00 -0400

    Refactor document#add_terminology_methods! into the terminology decorator

--- lib/nom/xml/decorators/terminology.rb
+++ lib/nom/xml/decorators/terminology.rb
@@ -4,8 +4,46 @@ module Nom::XML::Decorators::Terminology
     klass.add_terminology_methods!
   end
 
+  ##
+  # Add terminology accessors for querying child terms
   def add_terminology_methods!
-    document.add_terminology_methods(self)
+    terms_to_add = self.terms
+    self.ancestors.each do |a|
+      a.terms.each { |k,t| terms_to_add[k] ||= t if t.options[:global] }
+    end
+
+    terms_to_add.each do |k, t|
+      (class << self; self; end).send(:define_method, k.to_sym) do |*args|
+        options = args.extract_options!
+
+        args += options.map { |key, value| %{#{key}="#{value.gsub(/"/, '\\\"') }"} }
+
+        xpath = t.local_xpath
+
+        xpath += "[#{args.join('][')}]" unless args.empty?
+
+        result = case self
+                   when Nokogiri::XML::Document
+                     self.root.xpath(xpath, self.document.terminology_namespaces)
+                   else
+                     self.xpath(xpath, self.document.terminology_namespaces)
+                 end
+
+        m = t.options[:accessor]
+        case
+          when m.nil?
+            result
+          when m.is_a?(Symbol)
+            result.collect { |r| r.send(m) }
+          when m.is_a?(Proc)
+            result.collect { |r| m.call(r) }
+          else
+            raise "Unknown accessor class: #{m.class}"
+        end
+      end
+    end
+
+    self
   end
 
   def terms
@@ -20,11 +58,11 @@ module Nom::XML::Decorators::Terminology
 
   private
 
-  def root_terms 
+  def root_terms
     self.document.terminology.terms
   end
 
-  def child_terms 
+  def child_terms
     h = {}
 
     collect_terms_for_node.each do |k,v|
--- lib/nom/xml/nokogiri_extension.rb
+++ lib/nom/xml/nokogiri_extension.rb
@@ -30,46 +30,5 @@ module Nom::XML
       @terminology ||= Nom::XML::Terminology.new
     end
 
-    ##
-    # Add terminology accessors for querying child terms
-    # @param [Nokogiri::XML::Node] node
-    def add_terminology_methods node
-      terms_to_add = node.terms
-      node.ancestors.each do |a|
-        a.terms.each { |k,t| terms_to_add[k] ||= t if t.options[:global] }
-      end
-
-      terms_to_add.each do |k, t|
-        (class << node; self; end).send(:define_method, k.to_sym) do |*args|
-          options = args.extract_options!
-
-          args += options.map { |key, value| %{#{key}="#{value.gsub(/"/, '\\\"') }"} }
-
-          xpath = t.local_xpath
-
-
-          xpath += "[#{args.join('][')}]" unless args.empty?
-
-          result = case self
-                     when Nokogiri::XML::Document
-                       self.root.xpath(xpath, node.document.terminology_namespaces)
-                     else
-                       self.xpath(xpath, node.document.terminology_namespaces)
-                   end
-
-          m = t.options[:accessor]
-          case
-          when m.nil?
-            result
-          when m.is_a?(Symbol)
-            result.collect { |r| r.send(m) }
-          when m.is_a?(Proc)
-            result.collect { |r| m.call(r) }
-          else
-            raise "Unknown accessor class: #{m.class}"
-          end
-        end
-      end
-    end
   end
 end
