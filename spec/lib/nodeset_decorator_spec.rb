76b08cfd87960075d4ac6c70ade203d09d7c6787
Joe Atzberger <ohiocore@gmail.com>
2015-02-05 15:54:38 -0800

    Update respond_to for ruby 2.0, fixes remaining failing test

--- spec/lib/nodeset_decorator_spec.rb
+++ spec/lib/nodeset_decorator_spec.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
 
 describe Nom::XML::Decorators::NodeSet do
-  	subject { 
-      doc = Nokogiri::XML '<root><a n="1"/><b /><c /></root>'
-      doc.nom!
-      doc
-  	}
+  subject {
+    doc = Nokogiri::XML '<root><a n="1"/><b /><c /></root>'
+    doc.nom!
+    doc
+  }
   describe "#values_for_term" do
 
-
     it "should do if" do
-      t = double(:options => { :if => lambda { |x| false }})
-
+      t  = double(:options => { :if => lambda { |x| false }})
       t1 = double(:options => { :if => lambda { |x| true }})
 
       expect(subject.xpath('//*').values_for_term(t)).to be_empty
@@ -19,8 +17,7 @@ describe Nom::XML::Decorators::NodeSet do
     end
 
     it "should do unless" do
-      t = double(:options => { :unless => lambda { |x| false }})
-
+      t  = double(:options => { :unless => lambda { |x| false }})
       t1 = double(:options => { :unless => lambda { |x| true }})
 
       expect(subject.xpath('//*').values_for_term(t)).not_to be_empty
@@ -29,7 +26,6 @@ describe Nom::XML::Decorators::NodeSet do
 
     it "should do a nil accessor" do
       t = double(:options => { :accessor => nil})
-
       expect(subject.xpath('//*').values_for_term(t)).to eq(subject.xpath('//*'))
     end
 
@@ -39,21 +35,19 @@ describe Nom::XML::Decorators::NodeSet do
     end
 
     it "should do a symbol accessor" do
-      t = double(:options => { :accessor => :z})
-      expect(subject.xpath('//a').first).to receive(:z).and_return(1)
+      t = double(:options => { :accessor => :z })
+      allow(subject.xpath('//a').first).to receive(:z).and_return(1)
       expect(subject.xpath('//a').values_for_term(t)).to eq([1])
     end
 
     it "should do single" do
       t = double(:options => { :single => true})
       expect(subject.xpath('//*').values_for_term(t)).to eq(subject.xpath('//*').first)
-
     end
 
     it "should treat an attribute as a single" do
       t = double(:options => { })
       expect(subject.xpath('//@n').values_for_term(t)).to be_a_kind_of Nokogiri::XML::Attr
-
     end
   end
 
