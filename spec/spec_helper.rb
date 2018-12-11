975e45a2ce860c0236cb0388c2c0801c89cbb578
Chris Beer <cabeer@stanford.edu>
2018-12-11 11:10:45 -0800

    Run simplecov unconditionally.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
-if ENV['COVERAGE'] and RUBY_VERSION =~ /^1.9/
-  require 'simplecov'
-  SimpleCov.start
-end
+require 'simplecov'
+SimpleCov.start
 
 require 'rspec'
 require 'nom'
 require 'equivalent-xml/rspec_matchers'
-
 RSpec.configure do |config|
 
 end
-
